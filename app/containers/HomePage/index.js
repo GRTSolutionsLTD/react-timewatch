7c75a21b4b17964535d013cec1dbbe76e9775a69
Lea Tzeiger <leat@grth.co.il>
2019-07-22 17:09:22 +0300

    final route

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,27 +7,15 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { NavLink } from 'react-router-dom';
-
 import messages from './messages';
 import './index.scss';
 
 export default function HomePage() {
-  const activeStyle = { color: '#F15B2A' };
   return (
     <>
       <h1>
         <FormattedMessage {...messages.header} />
       </h1>
-      <nav>
-        <NavLink to="/register" activeStyle={activeStyle} exact>
-          Register
-        </NavLink>
-        {' | '}
-        <NavLink to="/reports" activeStyle={activeStyle}>
-          Reports
-        </NavLink>
-      </nav>
     </>
   );
 }
